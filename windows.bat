7c9515b4c83c13f3bab516f556b8abdcae19ca95
Johnny O'Donnell <odjohnny@amazon.com>
2023-01-24 04:54:48 -0800

    Make sure bat script finishes
    See:
    https://stackoverflow.com/questions/27992930/why-does-my-bat-file-abruptly-end-after-calling-another-bat-file

--- windows.bat
+++ windows.bat
@@ -3,5 +3,7 @@ if not exist "C:\Users\%USERNAME%\tmp" mkdir C:\Users\%USERNAME%\tmp
 if exist "C:\Users\%USERNAME%\tmp\terminal-config-initial" rmdir /s /q C:\Users\%USERNAME%\tmp\terminal-config-initial
 git clone --depth=1 git@github.com:johnnyodonnell/terminal-config.git C:\Users\%USERNAME%\tmp\terminal-config-initial
 
-C:\Users\%USERNAME%\tmp\terminal-config-initial\installers\windows.bat && rmdir /s /q C:\Users\%USERNAME%\tmp\terminal-config-initial
+call C:\Users\%USERNAME%\tmp\terminal-config-initial\installers\windows.bat 
+
+rmdir /s /q C:\Users\%USERNAME%\tmp\terminal-config-initial
 
