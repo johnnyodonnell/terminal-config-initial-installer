37555b89224d8c2faa42385e33c7ca97aa76fd78
Johnny O'Donnell <odjohnny@amazon.com>
2023-01-24 05:00:36 -0800

    Add comment about using the 'call' command

--- windows.bat
+++ windows.bat
@@ -3,6 +3,8 @@ if not exist "C:\Users\%USERNAME%\tmp" mkdir C:\Users\%USERNAME%\tmp
 if exist "C:\Users\%USERNAME%\tmp\terminal-config-initial" rmdir /s /q C:\Users\%USERNAME%\tmp\terminal-config-initial
 git clone --depth=1 git@github.com:johnnyodonnell/terminal-config.git C:\Users\%USERNAME%\tmp\terminal-config-initial
 
+:: Using 'call' after reading:
+:: https://stackoverflow.com/a/27993017/5832619
 call C:\Users\%USERNAME%\tmp\terminal-config-initial\installers\windows.bat 
 
 rmdir /s /q C:\Users\%USERNAME%\tmp\terminal-config-initial
