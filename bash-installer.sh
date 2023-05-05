243ac30dc59be93c80c9d009991eee0c63999d2b
Johnny O'Donnell <odjohnny@amazon.com>
2023-05-05 14:34:23 -0700

    Copy changes from zsh installer to bash installer

--- bash-installer.sh
+++ bash-installer.sh
@@ -5,10 +5,10 @@ echo Which installer would you like to run?
 read installer
 echo ""
 
-mkdir /tmp/terminal-config-initial
+mkdir -p /tmp/terminal-config-initial
 cd /tmp/terminal-config-initial
 
-rm -rf /tmp/terminal-config
+rm -rf /tmp/terminal-config-initial/terminal-config
 git clone --depth=1 git@github.com:johnnyodonnell/terminal-config.git
 
 /tmp/terminal-config-initial/terminal-config/installers/$installer
