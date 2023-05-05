f6dd40f0d6a370c1914b9aab371266e2f4de5a7c
Johnny O'Donnell <odjohnny@amazon.com>
2023-05-05 14:14:59 -0700

    Make linux installers generic

---
+++ zsh-installer.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+
+echo Which installer would you like to run?
+read installer
+echo ""
+
+mkdir /tmp/terminal-config-initial
+cd /tmp/terminal-config-initial
+
+rm -rf /tmp/terminal-config
+git clone --depth=1 git@github.com:johnnyodonnell/terminal-config.git
+
+/tmp/terminal-config-initial/terminal-config/installers/$installer
+
+rm -rf /tmp/terminal-config-initial
+
